50160513f7fbc95bce84ffbeb0e2674dfd13839f
g-wagen <>
2021-12-09 17:43:04 +0100

    removed inner for loops. but still not enough memory for the second challenge

--- 06/aoc_2021_6_2.py
+++ 06/aoc_2021_6_2.py
@@ -1,14 +1,16 @@
-import numpy
 import helper
 import numpy as np
+import gc
+
+gc.collect()
 
 data = ['3,4,3,1,2']
 data = helper.get_puzzle_input(d=6, y=2021)
-data = np.array([int(x) for x in data[0].split(',')])
+data = np.array([int(x) for x in data[0].split(',')]).astype('int64')
 
 prev = []
 
-days = 80
+days = 180
 
 for d in range(1, days + 1):
     prev = data.copy()
@@ -21,9 +23,15 @@ for d in range(1, days + 1):
 
     compare = np.array([prev_has_zero, data_has_six]).T
     simple = np.all(compare, axis=1)
-    add_multiplier = len(simple[simple == True])
+    add_multiplier = simple[simple == True].shape[0]
 
-    data = np.append(data, [8]*add_multiplier)
+    data = np.append(data, np.array([8] * add_multiplier))
+    del prev_has_zero
+    del data_has_six
+    del compare
+    del simple
+    del add_multiplier
+    gc.collect()
 
 print(f'Lanternfish population after {days} days: {len(data)}')
 
