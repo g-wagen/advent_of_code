c309bd4279295c0066f73b7c167b25fe365407d1
g-wagen <79096832+g-wagen@users.noreply.github.com>
2021-12-09 17:55:09 +0100

    12 06 02 001 (#10)
    * added some code to automatically fetch the puzzle input
    
    * day 6 part 1 done
    
    * removed inner for loops. but still not enough memory for the second challenge
    
    * removed garbage collection from script
    
    Co-authored-by: g-wagen <>

---
+++ 06/aoc_2021_6_2.py
@@ -0,0 +1,28 @@
+import helper
+import numpy as np
+
+data = ['3,4,3,1,2']
+data = helper.get_puzzle_input(d=6, y=2021)
+data = np.array([int(x) for x in data[0].split(',')]).astype('int64')
+
+prev = []
+
+days = 180
+
+for d in range(1, days + 1):
+    prev = data.copy()
+    data -= 1
+
+    data = np.where(data == -1, 6, data)
+
+    prev_has_zero = prev == 0
+    data_has_six = data == 6
+
+    compare = np.array([prev_has_zero, data_has_six]).T
+    simple = np.all(compare, axis=1)
+    add_multiplier = simple[simple == True].shape[0]
+
+    data = np.append(data, np.array([8] * add_multiplier))
+
+print(f'Lanternfish population after {days} days: {len(data)}')
+
