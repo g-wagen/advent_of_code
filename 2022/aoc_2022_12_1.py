f7ae2b8634d2aac222314b87354f16aec7420b39
g-wagen <79096832+g-wagen@users.noreply.github.com>
2023-12-04 00:09:27 +0100

    chore: rename 2022-12 input sample file

--- 2022/aoc_2022_12_1.py
+++ 2022/aoc_2022_12_1.py
@@ -1,77 +1,36 @@
-import numpy as np
-from collections import deque
+from helper import choose_puzzle_input, print_solution
+from string import ascii_lowercase
 
-from helper import choose_puzzle_input
-
-# puzzle_input = [
-#     "Sabqponm",
-#     "abcryxxl",
-#     "accszExk",
-#     "acctuvwj",
-#     "abdefghi"
-# ]
+year = 2022
+day = 12
 
 puzzle_input = choose_puzzle_input(
-    y=2022, d=12, sample_input_path="aoc_2022_12_inputsample.txt"
+    y=year,
+    d=day,
+    sample_input_path=f"aoc_{year}_{day:02d}_input_sample.txt",
 )
-# puzzle_input = choose_puzzle_input(y=2022, d=12)
-character = ord("a")
-print(puzzle_input)
+move_order = f"S{ascii_lowercase}E"
+
+moves_map = {c: d for c, d in zip(move_order, range(len(move_order)))}
+print(moves_map)
+
+
 data = []
-for line in puzzle_input:
-    new_line = []
-    for char in line:
-        if char.islower():
-            new_line.append(1 + ord(char) - character)
-        if char == "S":
-            new_line.append(0)
-        if char == "E":
-            new_line.append(27)
-    data.append(new_line)
+start = None
+end = None
 
+for l, line in enumerate(puzzle_input):
+    print(line)
+    find_start = line.find("S")
+    find_end = line.find("E")
+    if find_start > -1:
+        start = [l, find_start]
+    elif find_end > -1:
+        end = [l, find_end]
+    else:
+        pass
 
-print(data)
+print(start)
+print(end)
 
-
-def find_paths_recursive(grid, current_path=[(0, 0)], solutions=[]):
-    n = len(grid)
-    dirs = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-
-    last_cell = current_path[-1]
-
-    for x, y in dirs:
-        new_i = last_cell[0] + x
-        new_j = last_cell[1] + y
-
-        # Check if new cell is in grid
-        if new_i < 0 or new_i >= n or new_j < 0 or new_j >= n:
-            continue
-
-        # Check if new cell is already in path
-        if (new_i, new_j) in current_path:
-            continue
-
-        # Add cell to current path
-        current_path_copy = current_path.copy()
-        current_path_copy.append((new_i, new_j))
-
-        if new_i == n - 1 and new_j == n - 1:
-            continue
-
-        # Check if new cell has bigger value than last
-        if grid[new_i][new_j] > grid[last_cell[0]][last_cell[1]]:
-            solutions.append(current_path_copy)
-
-        # Create new current_path array for every direction
-        find_paths_recursive(grid, current_path_copy, solutions)
-
-    return solutions
-
-
-solutions = find_paths_recursive(data)
-
-lenghts = set()
-for solution in solutions:
-    lenghts.add(len(solution))
-
-print(min(lenghts))
+print_solution(solution=0, y=year, d=day, part=1)
