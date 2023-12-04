e4d4dcad87d1183f3ecebbf52f3b60f7455c4227
g-wagen <79096832+g-wagen@users.noreply.github.com>
2023-12-05 00:13:41 +0100

    solve 2023 day 3 part 1

--- 2023/aoc_2023_03_1.py
+++ 2023/aoc_2023_03_1.py
@@ -1,4 +1,3 @@
-from typing import Union
 from helper import choose_puzzle_input, print_solution
 
 year = 2023
@@ -7,230 +6,89 @@ day = 3
 puzzle_input = choose_puzzle_input(
     y=year,
     d=day,
-    sample_input_path=f"aoc_{year}_{day:02d}_input_sample.txt",
+    # sample_input_path=f"aoc_{year}_{day:02d}_input_sample.txt",
 )
 
 
-# 1. input als 2d liste speichern  v/
-# 2. zahlen zu int konvertieren  v/
-# 3. alle symbole finden und index speichern  v/
-# 4. durch symbole iterieren:
-#   - in koordinaten schauen ob da zahlen stehen (diagonal senkrecht, vertikal)
-#       - U
-#       - UR
-#       - R
-#       - DR
-#       - D
-#       - DL
-#       - L
-#       - UL
-# 5. Wenn zahl gefunden:
-#       - U: 1. ab X index von U in oberer zeile iterieren iterieren bis keine zahl mehr
-#            und in list/deque appenden
-#            1. ab X-1 index von U in oberer Zeile rückwärts iterieren bis keine Zahl und
-#            an andere liste appenden
-#            3. neue liste aus umgedrehter zweiter liste + erster liste
-#            4. nummer als Part Nummer an part nummer liste anfügen
-
-symbols = []
-part_numbers = []
 schematic = []
-
+all_numeric_ranges = []
+all_numeric_indices = []
 part_number_chars = ""
+total = []
 
 
-# puzzle_input = [
-#     '.........',
-#     '.360*201.',
-#     '.........'
-# ]
-
+# All 2d indices of numeric characters
 for y, line in enumerate(puzzle_input):
-    # print(line)
-    temp_list = []
+    numerics_indices = []
     for x, char in enumerate(line):
         if char.isnumeric():
-            temp_list.append(char)
+            numerics_indices.append([y, x])
         elif char == ".":
-            temp_list.append(" ")
-        else:
-            temp_list.append(char)
-            symbols.append([char, y, x])
-            if char not in part_number_chars:
-                part_number_chars += char
-
-    schematic.append(temp_list)
-
-print(part_number_chars)
-
-
-def lowest_index(input_list: list[list], the_line: str, funky_chars: str):
-    lowest = len(input_list[0])
-    for char in funky_chars:
-        char_id = the_line.find(char)
-        if char_id < 0:
             continue
-        elif char_id < lowest:
-            lowest = char_id
-    return lowest
+        elif char not in part_number_chars:
+            part_number_chars += char
+    all_numeric_indices.append(numerics_indices)
+
+# slice indices of found numbers
+for line in all_numeric_indices:
+    numeric_range, beginnings, ends = [], [], []
+    for i, item in enumerate(line):
+        if i == 0:
+            beginnings.append(item[1])
+        elif i == len(line) - 1:
+            ends.append(item[1])
+        elif item[1] != line[i - 1][1] + 1:
+            ends.append(line[i - 1][1])
+            beginnings.append(item[1])
+
+    for b, e in zip(beginnings, ends):
+        numeric_range.append([b, e])
+
+    all_numeric_ranges.append(numeric_range)
 
 
-def read_from_index(input_array: list[list], y: int, x: int):
-    cur_line = "".join(input_array[y])
+def has_symbols(puzzle, sym: str, y: int, x1: int, x2: int) -> bool:
+    left = x1 - 1
+    right = x2 + 1
+    above = y - 1
+    below = y + 1
 
-    forward_line = cur_line[x:]
-    backward_line = cur_line[:x][::-1]
-    non_digit_chars = part_number_chars + " "
+    chars, has_it = "", []
 
-    # line beginning
-    forward_lowest = lowest_index(
-        input_list=input_array,
-        the_line=forward_line,
-        funky_chars=non_digit_chars,
+    # links
+    chars += puzzle[y][left] if left >= 0 else ""
+    # rechts
+    chars += puzzle[y][right] if right < len(puzzle[y]) else ""
+    # oben links
+    chars += puzzle[above][left] if above >= 0 and left >= 0 else ""
+    # oben rechts
+    chars += puzzle[above][right] if above >= 0 and right < len(puzzle[y]) else ""
+    # unten links
+    chars += puzzle[below][left] if below < len(puzzle) and left >= 0 else ""
+    # unten rechts
+    chars += (
+        puzzle[below][right] if below < len(puzzle) and right < len(puzzle[y]) else ""
     )
+    # oben
+    chars += puzzle[above][left:right] if above >= 0 else ""
+    # unten
+    chars += puzzle[below][left:right] if below < len(puzzle) else ""
 
-    beginning = forward_line[:forward_lowest]
+    for s in sym:
+        if s in chars:
+            has_it.append(True)
+        else:
+            has_it.append(False)
 
-    # line end
-    backward_lowest = lowest_index(
-        input_list=input_array,
-        the_line=backward_line,
-        funky_chars=non_digit_chars,
-    )
-
-    end = backward_line[:backward_lowest]
-
-    if len(beginning) == 1:
-        part_number = end[::-1] + beginning
-    elif len(end) == 1:
-        part_number = end + beginning
-    else:
-        part_number = beginning + end
-
-    return int(part_number.strip())
+    return any(has_it)
 
 
-adjacents = []
-check_these = []
-for s in symbols:
-    sy = s[1]
-    sx = s[2]
-
-    u_y = sy - 1
-    d_y = sy + 1
-    r_x = sx + 1
-    l_x = sx - 1
-
-    # for direction in [
-    #     [u_y, sx],
-    #     [u_y, r_x],
-    #     [sy, r_x],
-    #     [d_y, r_x],
-    #     [d_y, sx],
-    #     [d_y, l_x],
-    #     [sy, l_x],
-    #     [u_y, l_x],
-    # ]:
-    #     try:
-    #         content = schematic[direction[0]][direction[1]]
-    #         if content.isnumeric():
-    #             check_these.append([content[0], content[1]])
-    #     except:
-    #         content = None
-    try:
-        u = schematic[u_y][sx]
-        if u.isnumeric():
-            check_these.append([u_y, sx])
-    except IndexError:
-        u = None
-    try:
-        ur = schematic[u_y][r_x]
-        if ur.isnumeric():
-            check_these.append([u_y, r_x])
-    except IndexError:
-        ur = None
-    try:
-        r = schematic[sy][r_x]
-        if r.isnumeric():
-            check_these.append([sy, r_x])
-    except IndexError:
-        r = None
-    try:
-        dr = schematic[d_y][r_x]
-        if dr.isnumeric():
-            check_these.append([d_y, r_x])
-    except IndexError:
-        dr = None
-    try:
-        d = schematic[d_y][sx]
-        if d.isnumeric():
-            check_these.append([d_y, sx])
-    except IndexError:
-        d = None
-    try:
-        dl = schematic[d_y][l_x]
-        if dl.isnumeric():
-            check_these.append([d_y, l_x])
-    except IndexError:
-        dl = None
-    try:
-        l = schematic[sy][l_x]
-        if l.isnumeric():
-            check_these.append([sy, l_x])
-    except IndexError:
-        l = None
-    try:
-        ul = schematic[u_y][l_x]
-        if ul.isnumeric():
-            check_these.append([u_y, l_x])
-    except IndexError:
-        ul = None
-
-print(check_these)
-
-for sch in schematic:
-    print(sch)
+for y, anr in enumerate(all_numeric_ranges):
+    for part in anr:
+        if has_symbols(
+            puzzle=puzzle_input, sym=part_number_chars, y=y, x1=part[0], x2=part[1]
+        ):
+            total.append(int(puzzle_input[y][part[0] : part[1] + 1]))
 
 
-# for i in [
-#     [0, 2, 467],
-#     [0, 0, 467],
-#     [0, 1, 467],
-#     [2, 2, 35],
-#     [2, 3, 35],
-#     [2, 6, 633],
-#     [2, 7, 633],
-#     [2, 8, 633],
-#     [4, 0, 617],
-#     [4, 1, 617],
-#     [4, 2, 617],
-#     [6, 2, 592],
-#     [6, 3, 592],
-#     [6, 4, 592],
-#     [7, 6, 755],
-#     [7, 7, 755],
-#     [7, 8, 755],
-#     [9, 1, 664],
-#     [9, 2, 664],
-#     [9, 3, 664],
-#     [9, 5, 598],
-#     [9, 6, 598],
-#     [9, 7, 598],
-# ]:
-#     result = read_from_index(input_array=schematic, y=i[0], x=i[1])
-#     print("TEST", i[2], result, i[2] == result)
-
-part_numbers = []
-for p in part_numbers:
-    print(p)
-
-for coords in check_these:
-    part_numbers.append(
-        read_from_index(input_array=schematic, y=coords[0], x=coords[1])
-    )
-
-
-print_solution(solution=sum(list(set(part_numbers))), y=year, d=day, part=1)
-# LOW:  336560
-# HIGH: 847901
-print_solution(solution=0, y=year, d=day, part=2)
+print_solution(solution=sum(total), y=year, d=day, part=1)
