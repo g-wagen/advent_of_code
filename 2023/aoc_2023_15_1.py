c2cb4fbb02d3e68e2ee02d9fbc5376dbb90e6ae7
g-wagen <79096832+g-wagen@users.noreply.github.com>
2023-12-27 15:08:23 +0100

    feat: solve and refactor 2023 day 15 part 1 and 2

--- 2023/aoc_2023_15_1.py
+++ 2023/aoc_2023_15_1.py
@@ -9,15 +9,16 @@ puzzle_input = choose_puzzle_input(
     # sample_input_path=f"aoc_{year}_{day:02d}_input_sample.txt",
 )
 
-values = []
 
-for i, wtf in enumerate(puzzle_input[0].split(",")):
-    current = 0
-    for w in wtf:
-        current += ord(w)
-        current *= 17
-        current = current % 256
-    values.append(current)
+def hash_alg(sequence: str) -> int:
+    out = 0
+    for item in sequence:
+        out += ord(item)
+        out *= 17
+        out = out % 256
+    return out
 
+
+values = [hash_alg(wtf) for wtf in puzzle_input[0].split(",")]
 
 print_solution(solution=sum(values), y=year, d=day, part=1)
