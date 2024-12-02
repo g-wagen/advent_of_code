3624b9e9b5f669a565f747927679c799c46c91f3
Anton Bauer <a.bauer@simplicity.ag>
2024-12-02 14:09:38 +0100

    refactor: go knows functions too :D

--- go/2024/02-1.go
+++ go/2024/02-1.go
@@ -1,84 +1,67 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "slices"
-    "strings"
-    "sort"
-    "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"sort"
+	"strconv"
+	"strings"
 )
 
+func reportsafe(levels []int) bool {
+	if sort.IntsAreSorted(levels) {
+		safe := 0
+		optimal := len(levels) - 1
+
+		for i, v := range levels {
+			if i > 0 && v-levels[i-1] <= 3 && v-levels[i-1] >= 1 {
+				safe++
+			}
+		}
+
+		if optimal == safe {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
-    file, error := os.Open("02-input.txt")
-    
-    if error != nil {
-        fmt.Println(error)
-        return
-    }
-    defer file.Close()
+	file, error := os.Open("02-input.txt")
 
-    scanner := bufio.NewScanner(file)
+	if error != nil {
+		fmt.Println(error)
+		return
+	}
+	defer file.Close()
 
-    total := 0
+	scanner := bufio.NewScanner(file)
 
-    for scanner.Scan() {
-        fields := strings.Fields(scanner.Text())
-        fieldsi := []int{}
+	total := 0
 
-        for _, v := range fields {
-            iv, _ := strconv.Atoi(v)
-            fieldsi = append(fieldsi, iv)
-        }
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		fieldsi := []int{}
 
-        fieldsir := []int{}
-        
-        for _, v := range fieldsi {
-            fieldsir = append(fieldsir, v)
-        }
-        
-        slices.Reverse(fieldsir)
-        
-        if sort.IntsAreSorted(fieldsi) {
-            fmt.Println(fieldsi, "ascending")
-            
-            lastnum := fieldsi[0]
-            safe := 0
-            optimal := len(fields)-1
+		for _, v := range fields {
+			iv, _ := strconv.Atoi(v)
+			fieldsi = append(fieldsi, iv)
+		}
 
-            for i, v := range fieldsi {
-                if i > 0 && v - fieldsi[i-1] <= 3 && v - fieldsi[i-1] >= 1 {
-                    safe++
-                }
-                lastnum = v
-            }
-            fmt.Println(optimal, safe, lastnum)
+		fieldsir := []int{}
 
-            if optimal == safe {
-                total++
-            }
-        }
+		for _, v := range fieldsi {
+			fieldsir = append(fieldsir, v)
+		}
 
-        if sort.IntsAreSorted(fieldsir) {
-            fmt.Println(fieldsi, "descending")
-            
-            lastnum := fieldsir[0]
-            safe := 0
-            optimal := len(fields)-1
+		slices.Reverse(fieldsir)
 
-            for i, v := range fieldsir {
-                if i > 0 && v - fieldsir[i-1] <= 3 && v - fieldsir[i-1] >= 1 {
-                    safe++
-                }
-                lastnum = v
-            }
-            fmt.Println(optimal, safe, lastnum)
+		if reportsafe(fieldsi) || reportsafe(fieldsir) {
+			total++
+		}
 
-            if optimal == safe {
-                total++
-            }
-        }
-    }
-    fmt.Println(total)
+	}
+	fmt.Println(total)
 }
