40be5499cdd98fc5ab3e1346e61e2215c40085af
g-wagen <79096832+g-wagen@users.noreply.github.com>
2024-12-07 02:08:14 +0100

    not yet figured out correct sorting for day 05 part 2

--- go/2024/day-05.go
+++ go/2024/day-05.go
@@ -18,6 +18,20 @@ func splitByChar(line, sep string) []int {
 	return output
 }
 
+func updateInCorrectOrder(update []int, rules [][]int) bool {
+	pairsNumber := len(update) - 1
+	correctCount := 0
+	for p := 0; p < pairsNumber; p++ {
+		pair := []int{update[p], update[p+1]}
+		for _, rule := range rules {
+			if slices.Equal(rule, pair) {
+				correctCount++
+			}
+		}
+	}
+	return pairsNumber == correctCount
+}
+
 func day05part1(file *os.File) int {
 	scanner := bufio.NewScanner(file)
 	total := 0
@@ -36,22 +50,10 @@ func day05part1(file *os.File) int {
 	}
 
 	for _, update := range updates {
-		pairsNumber := len(update) - 1
-		correctCount := 0
-		for p := 0; p < pairsNumber; p++ {
-			pair := []int{update[p], update[p+1]}
-			for _, rule := range rules {
-				if slices.Equal(rule, pair) {
-					correctCount++
-				}
-			}
-
-		}
-		if pairsNumber == correctCount {
+		if updateInCorrectOrder(update, rules) == true {
 			total += update[(len(update)-1)/2]
 		}
 	}
-
 	return total
 }
 
@@ -59,12 +61,33 @@ func day05part2(file *os.File) int {
 	scanner := bufio.NewScanner(file)
 	total := 0
 
-	canvas := [][]string{}
+	rules := [][]int{}
+	updates := [][]int{}
 
 	for scanner.Scan() {
-		canvas = append(canvas, strings.Split(scanner.Text(), ""))
+		currentLine := scanner.Text()
+		if strings.Contains(currentLine, "|") {
+			rules = append(rules, splitByChar(currentLine, "|"))
+		}
+		if strings.Contains(currentLine, ",") {
+			updates = append(updates, splitByChar(currentLine, ","))
+		}
 	}
 
+	incorrectUpdates := [][]int{}
+
+	for _, update := range updates {
+		if updateInCorrectOrder(update, rules) == false {
+			incorrectUpdates = append(incorrectUpdates, update)
+		}
+	}
+
+    for _, update := range updates {
+        for updateInCorrectOrder(update, rules) == false {
+            fmt.Println("figure out the correct sorting order")
+        }
+		total += update[(len(update)-1)/2]
+    }
 	return total
 }
 
