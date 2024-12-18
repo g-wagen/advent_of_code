2eb264d3fdd3bfd7b1f636eb649176ee4ab2f7f0
g-wagen <79096832+g-wagen@users.noreply.github.com>
2024-12-18 23:48:12 +0100

    solve day 8 part 2

--- go/2024/8.go
+++ go/2024/8.go
@@ -21,6 +21,17 @@ func permutate(slice [][]int) [][][]int {
 	return out
 }
 
+func readInput(file *os.File) []string{
+	output := []string{}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		output = append(output, scanner.Text())
+	}
+    return output
+}
+
 func d8p1(file *os.File) int {
 	total := 0
 
@@ -28,12 +39,10 @@ func d8p1(file *os.File) int {
 	antennas := []string{}
 	antennasPairs := make(map[string][][][]int)
 	antennasPositions := make(map[string][][]int)
-	antennasAntinodes := make(map[string][][]int)
 
 	for _, ant := range antennas {
 		antennasPairs[ant] = [][][]int{}
 		antennasPositions[ant] = [][]int{}
-		antennasAntinodes[ant] = [][]int{}
 	}
 
 	m := []string{}
@@ -51,7 +60,6 @@ func d8p1(file *os.File) int {
 		m = append(m, currentLine)
 	}
 
-	minXY := 0
 	maxY := len(m) - 1
 	maxX := len(m[0]) - 1
 
@@ -71,12 +79,12 @@ func d8p1(file *os.File) int {
 		permutations := permutate(positions)
 		pairs := [][][]int{}
 		for _, pair := range permutations {
-			c0, c1 := pair[0], pair[1]
-			x0, x1 := c0[0], c1[0]
+			pos0, pos1 := pair[0], pair[1]
+			x0, x1 := pos0[0], pos1[0]
 			// flip pair if first coordinate X
 			// is larger than second coordinate X
 			if x0 > x1 {
-				pair = [][]int{c1, c0}
+				pair = [][]int{pos1, pos0}
 			}
 			pairs = append(pairs, pair)
 		}
@@ -86,34 +94,34 @@ func d8p1(file *os.File) int {
 	// find all antinodes
 	antinodes := make(map[string]int)
 
-	for ant, pairs := range antennasPairs {
+	for _, pairs := range antennasPairs {
 		for _, pair := range pairs {
-			c0, c1 := pair[0], pair[1]
+			pos0, pos1 := pair[0], pair[1]
 
-			c0x, c0y := c0[0], c0[1]
-			c1x, c1y := c1[0], c1[1]
+			pos0x, pos0y := pos0[0], pos0[1]
+			pos1x, pos1y := pos1[0], pos1[1]
 
-			xDiff := int(math.Abs(float64(c1x - c0x)))
-			yDiff := int(math.Abs(float64(c1y - c0y)))
+			xDiff := int(math.Abs(float64(pos1x - pos0x)))
+			yDiff := int(math.Abs(float64(pos1y - pos0y)))
 
-			a0x := c0x - xDiff
-			a1x := c1x + xDiff
-			a0y, a1y := 0, 0
-			if c0y < c1y {
-				a0y = c0y - yDiff
-				a1y = c1y + yDiff
-			} else {
-				a0y = c0y + yDiff
-				a1y = c1y - yDiff
+			anode0x := pos0x - xDiff
+			anode1x := pos1x + xDiff
+			anode0y, anode1y := 0, 0
+			
+            anode0y = pos0y + yDiff
+			anode1y = pos1y - yDiff
+			
+            if pos0y < pos1y {
+				anode0y = pos0y - yDiff
+				anode1y = pos1y + yDiff
 			}
-			if a0x >= minXY && a0x <= maxX && a0y >= minXY && a0y <= maxY {
-				antinode := []int{a0x, a0y}
-				antennasAntinodes[ant] = append(antennasAntinodes[ant], antinode)
+
+			if anode0x >= 0 && anode0x <= maxX && anode0y >= 0 && anode0y <= maxY {
+				antinode := []int{anode0x, anode0y}
 				antinodes[fmt.Sprintf("%v", antinode)]++
 			}
-			if a1x >= minXY && a1x <= maxX && a1y >= minXY && a1y <= maxY {
-				antinode := []int{a1x, a1y}
-				antennasAntinodes[ant] = append(antennasAntinodes[ant], antinode)
+			if anode1x >= 0 && anode1x <= maxX && anode1y >= 0 && anode1y <= maxY {
+				antinode := []int{anode1x, anode1y}
 				antinodes[fmt.Sprintf("%v", antinode)]++
 			}
 		}
@@ -124,14 +132,151 @@ func d8p1(file *os.File) int {
 }
 
 func d8p2(file *os.File) int {
-	scanner := bufio.NewScanner(file)
 	total := 0
+
+	// initiate antenna data structures
+	antennas := []string{}
+	antennasPairs := make(map[string][][][]int)
+	antennasPositions := make(map[string][][]int)
+
+	for _, ant := range antennas {
+		antennasPairs[ant] = [][][]int{}
+		antennasPositions[ant] = [][]int{}
+	}
 
 	m := []string{}
 
+	scanner := bufio.NewScanner(file)
+
 	for scanner.Scan() {
-		m = append(m, scanner.Text())
+		currentLine := scanner.Text()
+		for _, char := range currentLine {
+			strChar := string(char)
+			if strChar != "." && slices.Contains(antennas, strChar) == false {
+				antennas = append(antennas, strChar)
+			}
+		}
+		m = append(m, currentLine)
 	}
+
+	maxY := len(m) - 1
+	maxX := len(m[0]) - 1
+
+	// find all antenna positions
+	for y, row := range m {
+		for x, col := range row {
+			strCol := string(col)
+			if strCol != "." {
+				position := []int{x, y}
+				antennasPositions[strCol] = append(antennasPositions[strCol], position)
+			}
+		}
+	}
+
+	// find all antenna pairs
+	for ant, positions := range antennasPositions {
+		permutations := permutate(positions)
+		pairs := [][][]int{}
+		for _, pair := range permutations {
+			pos0, pos1 := pair[0], pair[1]
+			x0, x1 := pos0[0], pos1[0]
+			// flip pair if first coordinate X
+			// is larger than second coordinate X
+			if x0 > x1 {
+				pair = [][]int{pos1, pos0}
+			}
+			pairs = append(pairs, pair)
+		}
+		antennasPairs[ant] = pairs
+	}
+
+	// find all antinodes
+	antinodes := make(map[string]int)
+
+	for _, pairs := range antennasPairs {
+		for _, pair := range pairs {
+			pos0, pos1 := pair[0], pair[1]
+
+			pos0x, pos0y := pos0[0], pos0[1]
+			pos1x, pos1y := pos1[0], pos1[1]
+
+			xDiff := int(math.Abs(float64(pos1x - pos0x)))
+			yDiff := int(math.Abs(float64(pos1y - pos0y)))
+
+			antinodes[fmt.Sprintf("%v", pos0)]++
+			antinodes[fmt.Sprintf("%v", pos1)]++
+
+			if pos0y < pos1y {
+				// ascending line
+				//
+				// go left down
+				ix, iy := pos0x, pos0y
+				for ix >= 0 && iy >= 0 {
+					antinode := []int{ix, iy}
+					antinodes[fmt.Sprintf("%v", antinode)]++
+					ix -= xDiff
+					iy -= yDiff
+				}
+				ix, iy = pos1x, pos1y
+				for ix >= 0 && iy >= 0 {
+					antinode := []int{ix, iy}
+					antinodes[fmt.Sprintf("%v", antinode)]++
+					ix -= xDiff
+					iy -= yDiff
+				}
+				// go right up
+				ix, iy = pos0x, pos0y
+				for ix <= maxX && iy <= maxY {
+					antinode := []int{ix, iy}
+					antinodes[fmt.Sprintf("%v", antinode)]++
+					ix += xDiff
+					iy += yDiff
+				}
+				ix, iy = pos1x, pos1y
+				for ix <= maxX && iy <= maxY {
+					antinode := []int{ix, iy}
+					antinodes[fmt.Sprintf("%v", antinode)]++
+					ix += xDiff
+					iy += yDiff
+				}
+			} else {
+				// descending line
+				//
+
+				// go left up
+				ix, iy := pos0x, pos0y
+				for ix >= 0 && iy <= maxY {
+					antinode := []int{ix, iy}
+					antinodes[fmt.Sprintf("%v", antinode)]++
+					ix -= xDiff
+					iy += yDiff
+				}
+				ix, iy = pos1x, pos1y
+				for ix >= 0 && iy <= maxY {
+					antinode := []int{ix, iy}
+					antinodes[fmt.Sprintf("%v", antinode)]++
+					ix -= xDiff
+					iy += yDiff
+				}
+				// go right down
+				ix, iy = pos0x, pos0y
+				for ix <= maxX && iy >= 0 {
+					antinode := []int{ix, iy}
+					antinodes[fmt.Sprintf("%v", antinode)]++
+					ix += xDiff
+					iy -= yDiff
+				}
+				ix, iy = pos1x, pos1y
+				for ix <= maxX && iy >= 0 {
+					antinode := []int{ix, iy}
+					antinodes[fmt.Sprintf("%v", antinode)]++
+					ix += xDiff
+					iy -= yDiff
+				}
+			}
+		}
+	}
+	total += len(antinodes)
 
 	return total
 }
