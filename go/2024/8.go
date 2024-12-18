c940625c512040e9be02007dfaacdcdb27ce6e21
g-wagen <79096832+g-wagen@users.noreply.github.com>
2024-12-18 21:30:03 +0100

    run formatter on day 8

--- go/2024/8.go
+++ go/2024/8.go
@@ -3,29 +3,28 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"slices"
 	_ "strconv"
 	_ "strings"
-    "math"
 )
 
-
-func permutate(slice [][]int) [][][]int{
-    out := [][][]int{}
+func permutate(slice [][]int) [][][]int {
+	out := [][][]int{}
 	for i := 0; i < len(slice)-1; i++ {
 		for j := i; j < len(slice)-1; j++ {
-            pair := [][]int{slice[i], slice[j+1]}
-            out = append(out, pair)
+			pair := [][]int{slice[i], slice[j+1]}
+			out = append(out, pair)
 		}
 	}
-    return out
+	return out
 }
 
 func d8p1(file *os.File) int {
 	total := 0
 
-    // initiate antenna data structures
+	// initiate antenna data structures
 	antennas := []string{}
 	antennasPairs := make(map[string][][][]int)
 	antennasPositions := make(map[string][][]int)
@@ -52,9 +51,9 @@ func d8p1(file *os.File) int {
 		m = append(m, currentLine)
 	}
 
-    minXY := 0
-    maxY := len(m)-1
-    maxX := len(m[0])-1
+	minXY := 0
+	maxY := len(m) - 1
+	maxX := len(m[0]) - 1
 
 	// find all antenna positions
 	for y, row := range m {
@@ -69,58 +68,57 @@ func d8p1(file *os.File) int {
 
 	// find all antenna pairs
 	for ant, positions := range antennasPositions {
-        permutations := permutate(positions)
-        pairs := [][][]int{}
-        for _, pair := range permutations {        
-            c0, c1 := pair[0], pair[1]
-            x0, x1 := c0[0], c1[0]
-            // flip pair if first coordinate X 
-            // is larger than second coordinate X
-            if x0 > x1 {
-                pair = [][]int{c1, c0}
-            } 
-            pairs = append(pairs, pair)
-        }
-		antennasPairs[ant] = pairs	
-    }
-    
+		permutations := permutate(positions)
+		pairs := [][][]int{}
+		for _, pair := range permutations {
+			c0, c1 := pair[0], pair[1]
+			x0, x1 := c0[0], c1[0]
+			// flip pair if first coordinate X
+			// is larger than second coordinate X
+			if x0 > x1 {
+				pair = [][]int{c1, c0}
+			}
+			pairs = append(pairs, pair)
+		}
+		antennasPairs[ant] = pairs
+	}
 
-    // find all antinodes
-    antinodes := make(map[string]int)
+	// find all antinodes
+	antinodes := make(map[string]int)
 
-    for ant, pairs := range antennasPairs {
-        for _, pair := range pairs {
-            c0, c1 := pair[0], pair[1]
+	for ant, pairs := range antennasPairs {
+		for _, pair := range pairs {
+			c0, c1 := pair[0], pair[1]
 
-            c0x, c0y := c0[0], c0[1]
-            c1x, c1y := c1[0], c1[1]
+			c0x, c0y := c0[0], c0[1]
+			c1x, c1y := c1[0], c1[1]
 
-            xDiff := int(math.Abs(float64(c1x - c0x)))
-            yDiff := int(math.Abs(float64(c1y - c0y)))
+			xDiff := int(math.Abs(float64(c1x - c0x)))
+			yDiff := int(math.Abs(float64(c1y - c0y)))
 
-            a0x := c0x - xDiff
-            a1x := c1x + xDiff
-            a0y, a1y := 0, 0
-            if c0y < c1y {
-                a0y = c0y - yDiff
-                a1y = c1y + yDiff
-            } else {
-                a0y = c0y + yDiff
-                a1y = c1y - yDiff
-            }
-            if a0x >= minXY && a0x <= maxX && a0y >= minXY && a0y <= maxY {
-                antinode := []int{a0x, a0y}
-                antennasAntinodes[ant] = append(antennasAntinodes[ant], antinode)
-                antinodes[fmt.Sprintf("%v", antinode)]++
-            }
-            if a1x >= minXY && a1x <= maxX && a1y >= minXY && a1y <= maxY {
-                antinode := []int{a1x, a1y}
-                antennasAntinodes[ant] = append(antennasAntinodes[ant], antinode)
-                antinodes[fmt.Sprintf("%v", antinode)]++
-            }
-        }
-    }
-    total += len(antinodes)
+			a0x := c0x - xDiff
+			a1x := c1x + xDiff
+			a0y, a1y := 0, 0
+			if c0y < c1y {
+				a0y = c0y - yDiff
+				a1y = c1y + yDiff
+			} else {
+				a0y = c0y + yDiff
+				a1y = c1y - yDiff
+			}
+			if a0x >= minXY && a0x <= maxX && a0y >= minXY && a0y <= maxY {
+				antinode := []int{a0x, a0y}
+				antennasAntinodes[ant] = append(antennasAntinodes[ant], antinode)
+				antinodes[fmt.Sprintf("%v", antinode)]++
+			}
+			if a1x >= minXY && a1x <= maxX && a1y >= minXY && a1y <= maxY {
+				antinode := []int{a1x, a1y}
+				antennasAntinodes[ant] = append(antennasAntinodes[ant], antinode)
+				antinodes[fmt.Sprintf("%v", antinode)]++
+			}
+		}
+	}
+	total += len(antinodes)
 
 	return total
 }
