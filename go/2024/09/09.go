8b504e46e44829cbebec416b1a13f95bedce3afb
g-wagen <79096832+g-wagen@users.noreply.github.com>
2024-12-22 23:53:50 +0100

    solve day 9 part 2

--- go/2024/09/09.go
+++ go/2024/09/09.go
@@ -44,13 +44,12 @@ func unpack(diskMap []int) []int {
 	return unpacked
 }
 
-func pack(unpacked []int) []int {
+func moveBlocks(unpacked []int) []int {
 	for slices.Index(unpacked, -1) > -1 {
 		s := slices.Index(unpacked, -1)
 		unpacked[s] = unpacked[len(unpacked)-1]
 		unpacked = unpacked[:len(unpacked)-1]
 	}
-
 	return unpacked
 }
 
@@ -66,20 +65,65 @@ func checksum(packed []int) int {
 	return sum
 }
 
+func count(s []int, n int) int {
+	out := 0
+	for _, item := range s {
+		if item == n {
+			out++
+		}
+	}
+	return out
+}
+
+func moveFiles(data []int) []int {
+	countdown := slices.Max(data)
+
+	for countdown > 0 {
+		need := count(data, countdown)
+		space := make([]int, need)
+
+		for i := range space {
+			space[i] = -1
+		}
+
+		arr := make([]int, need)
+		for i := 0; i < need; i++ {
+			arr[i] = countdown
+		}
+
+		index := slices.Index(data, countdown)
+
+		for i := 0; i < len(data); i++ {
+			window := data[i:]
+			have := window[:need]
+			if slices.Equal(have, space) && index > i {
+				for slices.Index(data, countdown) > -1 {
+					data[slices.Index(data, countdown)] = -1
+				}
+				data = slices.Replace(data, i, i+need, arr...)
+				break
+			}
+		}
+		countdown--
+	}
+	return data
+}
+
 func d9p1(file *os.File) int {
 	input := readInput(file)
 
 	unpacked := unpack(input)
-	packed := pack(unpacked)
+	moved := moveBlocks(unpacked)
 
-	return checksum(packed)
+	return checksum(moved)
 }
 
 func d9p2(file *os.File) int {
-	//input := readInput(file)
-	// puzzle input "00992111777.44.333....5555.6666.....8888.." transforms to this ...
-	packed := []int{0, 0, 9, 9, 2, 1, 1, 1, 7, 7, 7, -1, 4, 4, -1, 3, 3, 3, -1, -1, -1, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, -1, -1, -1, -1, 8, 8, 8, 8, -1, -1}
-	return checksum(packed) //2858
+	input := readInput(file)
+	unpacked := unpack(input)
+	moved := moveFiles(unpacked)
+
+	return checksum(moved)
 }
 
 func main() {
