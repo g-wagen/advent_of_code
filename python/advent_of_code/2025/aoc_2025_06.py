8f6ede8c0e1b58b6302085155f4d2b196a6b0959
g-wagen <79096832+g-wagen@users.noreply.github.com>
2025-12-11 22:00:29 +0100

    feat: solve 2025 day 6 part 2

--- python/advent_of_code/2025/aoc_2025_06.py
+++ python/advent_of_code/2025/aoc_2025_06.py
@@ -3,10 +3,7 @@ from advent_of_code.helper import print_solution, read_puzzle_input
 year = 2025
 day = 6
 
-# puzzle_input = read_puzzle_input(input_path="aoc_2025_06_input.txt")
-
-
-puzzle_input = read_puzzle_input(input_path="aoc_2025_06_input_sample.txt")
+puzzle_input = read_puzzle_input(input_path="aoc_2025_06_input.txt")
 
 
 def rotate_2d(pattern: list[list[str]]) -> list[list[str]]:
@@ -38,6 +35,43 @@ def d6p1() -> int:
 
 def d6p2() -> int:
     solution = 0
+
+    grid = []
+
+    for r, row in enumerate(puzzle_input):
+        oneline = []
+        for c, column in enumerate(row):
+            oneline.append(column)
+        grid.append(oneline)
+
+    rotated = rotate_2d(grid)
+
+    last_operation = ""
+    last_numbers = []
+
+    # add an empty row add the end
+    rotated.append([" " * len(rotated[0])])
+
+    for i, line in enumerate(rotated):
+        if line[0] in ["+", "*"]:
+            last_operation = line[0]
+
+        line_as_string = "".join([n for n in reversed(line[1:])])
+
+        try:
+            last_numbers.append(int(line_as_string))
+        except ValueError:
+            if last_operation == "+":
+                for number in last_numbers:
+                    solution += number
+
+            if last_operation == "*":
+                mult = 1
+                for number in last_numbers:
+                    mult *= number
+                solution += mult
+
+            last_numbers = []
 
     return solution
 
